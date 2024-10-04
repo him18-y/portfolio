9c99f768566fe9068276c2d5215cceec2983e53c
him18-y <hirayama1008yuta@icloud.com>
2024-10-04 14:09:26 +0900

    Add 404.html to fix GitHub Pages routing issue

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={Theme}>
-      <BrowserRouter basename="/portfolio">
+      <BrowserRouter basename="/portfolio/">
         <App />
       </BrowserRouter>
     </ThemeProvider>
